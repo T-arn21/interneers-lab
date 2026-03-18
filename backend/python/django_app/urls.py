3c00e4cfabc96fdf91b0a729b95790186418d2ca
T-arn21 <arnavtiku@gmail.com>
2026-03-18 20:17:26 +0530

    First edit

--- backend/python/django_app/urls.py
+++ backend/python/django_app/urls.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from django.urls import path
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
 def hello_world(request):
-    return HttpResponse("Hello, world! This is our interneers-lab Django server.")
+    # return HttpResponse("This is an updated message!")
+
+    name = request.GET.get("name","World")
+    # return JsonResponse({"message": f"Hello, {name}!"})
+
+    return HttpResponse(f"Hello {name}! This is an updated message")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
