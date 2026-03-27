d65eca09a0971dd234b230416313c615661932c9
T-arn21 <arnavtiku@gmail.com>
2026-03-27 23:30:49 +0530

    changes to posts app

--- backend/python/posts/urls.py
+++ backend/python/posts/urls.py
@@ -2,9 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.post_list, name= 'post_list'),
-    path('create/', views.create_post, name='create_post'),
-    path('get/', views.get_posts),
-    path('update/', views.update_post),
-    path('delete/', views.delete_post),
+    path("", views.landing_view, name="posts_landing"),
 ]
